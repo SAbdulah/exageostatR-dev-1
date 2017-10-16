3b60aa2c6301acd5885ae1bfa522bf47558c816b
Sameh Abdulah <sameh.abdulah@kaust.edu.sa>
2017-10-16 06:05:46 +0300

    update R-wrapper example inside the package

--- tests/r_zgen_mle_test.R
+++ tests/r_zgen_mle_test.R
@@ -1,11 +1,12 @@
-brary(exageostat)
-        theta1 = 1
+	install.packages(repos=NULL, "exageostat_0.1.0.tar.gz")
+        library(exageostat)
+	theta1 = 1
         theta2 = 0.1
         theta3 = 0.5
         computation = 0  #exact
         dmetric = 0     #ed
-        n=1600
-        ncores=3
+        n=19881
+        ncores=32
         gpus=0
         ts=320
         p_grid=1
@@ -20,4 +21,3 @@ brary(exageostat)
         vecs_out[1:globalveclen] = -1.99
         vecs_out = rexageostat_gen_zR(n, ncores, gpus, ts, p_grid, q_grid, theta1, theta2, theta3, computation, dmetric, globalveclen)
         theta_out = rexageostat_likelihoodR(n, ncores, gpus, ts, p_grid, q_grid,  vecs_out[1:n],  vecs_out[n+1:(2*n)],  vecs_out[(2*n+1):(3*n)], clb, cub, computation, dmetric)
-
