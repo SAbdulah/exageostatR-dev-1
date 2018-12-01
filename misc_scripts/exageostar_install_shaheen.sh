e0d67f8819ff6a76dfc393fedb5b24a7f57dafbf
sabdulah <sameh.abdulah@kaust.edu.sa>
2018-12-01 11:25:08 +0300

    fix

--- misc_scripts/exageostar_install_shaheen.sh
+++ misc_scripts/exageostar_install_shaheen.sh
@@ -1,28 +1,34 @@
-module load cmake/3.1.3
+module load cmake/3.10.2
 export LC_ALL=en_US.UTF-8
 export CRAYPE_LINK_TYPE=dynamic
-module switch PrgEnv-cray/5.2.82 PrgEnv-gnu
+module switch PrgEnv-cray PrgEnv-gnu
+#module load gsl/2.4
+#module load cray-netcdf
+#module load cray-hdf5
 #Intel MKL:
 #==========
-module load intel/16.3.3.210 
+module load intel/18.0.1.163
 
 SETUP_DIR=$PWD
 rm -rf exageostatr
-MKLROOT=/sw/csi/intel/2017/compilers_and_libraries/linux/mkl
 ==============================
 cd $SETUP_DIR
 if [ ! -d "nlopt-2.5.0" ]; then
-        wget https://github.com/stevengj/nlopt/archive/v2.5.0.tar.gz   nlopt-2.5.0.tar.gz
-        tar -zxvf nlopt-2.5.0.tar.gz
+        wget  https://github.com/stevengj/nlopt/archive/v2.5.0.tar.gz
+        tar -zxvf v2.5.0.tar.gz
 fi
 cd nlopt-2.5.0
-[[ -d nlopt_install ]] || mkdir nlopt_install
-CC=gcc ./configure --prefix=$PWD/nlopt_install/ --enable-shared --without-guile
+rm -rf build
+mkdir -p build/install_dir
+cd build
+
+cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_INSTALL_PREFIX=$PWD/install_dir/ -DBUILD_SHARED_LIBS=ON -DNLOPT_GUILE=OFF
 make -j
 make -j install
 NLOPTROOT=$PWD
-export PKG_CONFIG_PATH=$NLOPTROOT/nlopt_install/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$NLOPTROOT/nlopt_install/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$NLOPTROOT/install_dir/lib64/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$NLOPTROOT/install_dir/lib64:$LD_LIBRARY_PATH
+#export CPATH=$NLOPTROOT/install_dir/include:$CPATH
 ================================
 cd $SETUP_DIR
 if [  ! -d "hwloc-2.0.2" ]; then
@@ -46,13 +52,14 @@ if [ ! -d "starpu-1.2.6" ]; then
 fi
 cd starpu-1.2.6
 [[ -d starpu_install ]] || mkdir starpu_install
-CFLAGS=-fPIC CXXFLAGS=-fPIC CC=cc CXX=CC FC=ftn ./configure --prefix=$PWD/starpu_install/ --disable-cuda --disable-opencl --with-mpicc=/opt/cray/pe/craype/2.5.13/bin/cc --disable-shared --disable-build-doc --disable-export-dynamic --disable-mpi-check
+CFLAGS=-fPIC CXXFLAGS=-fPIC CC=cc CXX=CC FC=ftn ./configure --prefix=$PWD/starpu_install/ --disable-cuda --disable-opencl --with-mpicc=/opt/cray/pe/craype/2.5.13/bin/cc --enable-shared --disable-build-doc --disable-export-dynamic --disable-mpi-check
 make -j
 make -j  install
 STARPUROOT=$PWD
 export PKG_CONFIG_PATH=$STARPUROOT/starpu_install/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$STARPUROOT/starpu_install/lib:$LD_LIBRARY_PATH
-#************************************************************************ Install Chameleon - Stars-H - HiCMA 
+export CPATH=$STARPUROOT/starpu_install/include:$CPATH
+#************************************************************************ Install Chameleon - Stars-H - HiCMA
 cd $SETUP_DIR
 # Check if we are already in exageostat repo dir or not.
 if git -C $PWD remote -v | grep -q 'https://github.com/ecrc/exageostatr'
@@ -80,12 +87,12 @@ cd $STARSHDIR
 rm -rf build
 mkdir -p build/install_dir
 cd build
+CFLAGS=-fPIC cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DMPI=OFF -DOPENMP=OFF -DSTARPU=ON -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}"  -DEXAMPLES=OFF -DTESTING=OFF -DBUILD_SHARED_LIBS=ON
 
-CFLAGS=-fPIC cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DMPI=OFF -DOPENMP=OFF -DSTARPU=ON -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}"  -DEXAMPLES=OFF -DTESTING=OFF  
 make -j
 make install
-export PKG_CONFIG_PATH=$STARSHDIR/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$STARSHDIR/install_dir/lib/pkgconfig:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$STARSHDIR/build/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$STARSHDIR/build/install_dir/lib/pkgconfig:$LD_LIBRARY_PATH
 
 ## CHAMELEON
 cd $CHAMELEONDIR
@@ -94,14 +101,14 @@ mkdir -p build/install_dir
 cd build
 
 
-LDFLAGS=-lrt cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DCMAKE_COLOR_MAKEFILE:BOOL=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBUILD_SHARED_LIBS=ON -DCHAMELEON_ENABLE_EXAMPLE=ON -DCHAMELEON_ENABLE_TESTING=ON -DCHAMELEON_ENABLE_TIMING=ON -DCHAMELEON_USE_MPI=ON -DCHAMELEON_USE_CUDA=OFF -DCHAMELEON_USE_MAGMA=OFF -DCHAMELEON_SCHED_QUARK=OFF -DCHAMELEON_SCHED_STARPU=ON -DCHAMELEON_USE_FXT=OFF -DSTARPU_DIR=$STARPUROOT/starpu_install -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}" -DMORSE_VERBOSE_FIND_PACKAGE=ON -DMPI_C_COMPILER=/opt/cray/pe/craype/2.5.13/bin/cc    
+LDFLAGS=-lrt cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DCMAKE_COLOR_MAKEFILE:BOOL=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBUILD_SHARED_LIBS=ON -DCHAMELEON_ENABLE_EXAMPLE=ON -DCHAMELEON_ENABLE_TESTING=ON -DCHAMELEON_ENABLE_TIMING=ON -DCHAMELEON_USE_MPI=ON -DCHAMELEON_USE_CUDA=OFF -DCHAMELEON_USE_MAGMA=OFF -DCHAMELEON_SCHED_QUARK=OFF -DCHAMELEON_SCHED_STARPU=ON -DCHAMELEON_USE_FXT=OFF -DSTARPU_DIR=$STARPUROOT/starpu_install -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core;-lmkl_sequential;-lpthread;-lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}" -DMORSE_VERBOSE_FIND_PACKAGE=ON -DMPI_C_COMPILER=/opt/cray/pe/craype/2.5.13/bin/cc
 
 make -j # CHAMELEON parallel build seems to be fixed
 make install
 
-export PKG_CONFIG_PATH=$CHAMELEONDIR/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$CHAMELEONDIR/install_dir/lib/:$LD_LIBRARY_PATH
-
+export PKG_CONFIG_PATH=$CHAMELEONDIR/build/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$CHAMELEONDIR/build/install_dir/lib/:$LD_LIBRARY_PATH
+export CPATH=$CHAMELEONDIR/build/install_dir/include/coreblas:$CPATH
 
 ## HICMA
 cd $HICMADIR
@@ -110,13 +117,13 @@ mkdir -p build/install_dir
 cd build
 ===============
 
-LDFLAGS=-lrt cmake cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DHICMA_USE_MPI=1 -DCMAKE_COLOR_MAKEFILE:BOOL=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBUILD_SHARED_LIBS=OFF  $STARPUROOT/starpu_install -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core; -lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core; -lmkl_sequential;-lpthread; -lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}" -DMPI_C_COMPILER=/opt/cray/pe/craype/2.5.13/bin/cc
+cmake .. -DCMAKE_CXX_COMPILER=CC -DCMAKE_C_COMPILER=cc -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_INSTALL_PREFIX=$PWD/install_dir -DHICMA_USE_MPI=1 -DCMAKE_COLOR_MAKEFILE:BOOL=ON -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DBUILD_SHARED_LIBS=ON  -DSTARPU_DIR=$STARPUROOT/starpu_install -DBLAS_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core; -lmkl_sequential;-lpthread;-lm;-ldl" -DBLAS_COMPILER_FLAGS="-m64;-I${MKLROOT}/include" -DLAPACK_LIBRARIES="-Wl,--no-as-needed;-L${MKLROOT}/lib;-lmkl_intel_lp64;-lmkl_core; -lmkl_sequential;-lpthread; -lm;-ldl" -DCBLAS_DIR="${MKLROOT}" -DLAPACKE_DIR="${MKLROOT}" -DTMG_DIR="${MKLROOT}" -DMPI_C_COMPILER=/opt/cray/pe/craype/2.5.13/bin/cc
 
 make -j
 make install
 
-export PKG_CONFIG_PATH=$HICMADIR/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$HICMADIR/install_dir/lib/:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$HICMADIR/build/install_dir/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$HICMADIR/build/install_dir/lib/:$LD_LIBRARY_PATH
 
 $SETUP_DIR
 #export CPATH=$CPATH:/usr/local/include/coreblas && \
@@ -125,3 +132,12 @@ $SETUP_DIR
 
 ## Modify src/Makefile, compilation flagss -> flagsl
 
+
+
+module load gsl/2.4
+module load cray-netcdf
+module load cray-hdf5
+
+ module swap PrgEnv-gnu PrgEnv-cray
+ module load r/3.4.3
+ module swap PrgEnv-intel PrgEnv-gnu
